c6ba08ace01da9adfd1069f16dcccd7ddd669081
pjt3591oo <pjt3591oo@naver.com>
2015-11-24 10:58:13 +0900

    test1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('index', { title: 'Expressssssss' });
+});
+
 module.exports = router;
