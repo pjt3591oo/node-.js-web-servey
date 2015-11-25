dda4756e78b7aa361c3418e5a9ff94b08ddc8fe3
pjt3591oo <pjt3591oo@naver.com>
2015-11-26 07:59:15 +0900

    로그인, 회원가입 폼 추가

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var po = require('../models/Post');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+      res.render('index', { title: 'Express' });
+
+  });
+
+
+router.get('/signout', function(req, res, next) {
+     res.render('index', { title: 'Express' });
 });
 
-router.get('/new', function(req, res, next) {
-  res.render('index', { title: 'Expressssssss' });
+router.get('/signin', function(req, res, next) {
+
+    res.render('signin', { });
+
 });
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+*/
+router.get('/', function(req, res, next) {
+
+    res.render('index', { title: 'Express' });
+
+});
+
+
+router.get('/new', function(req, res, next) {
+
+    res.render('./users/new', { title: 'Express' });
+
+});
+
+
+router.get('/:id', function(req, res, next) {
+  po.find({},function(err,data){
+    if(err){
+      return next(err);
+    }else{
+      console.log(data);
+      res.render('index', { title: 'Express' });
+    }
+  });
+});
+
+router.get('/emailAuth', function(req, res, next) {
+
+  res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
