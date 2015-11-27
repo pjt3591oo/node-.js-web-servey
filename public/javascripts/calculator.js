93f4310689351d96fc929a11401ad2d3ebaa0af0
pjt3591oo <pjt3591oo@naver.com>
2015-11-27 09:29:14 +0900

    설문 작성 부분 양식,옵션 추가 및 추가부분 수정 필요

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,34 +1,21 @@
 $('document').ready(function(){
 
-	$('#add').click(function() {
-		$(".optionList").append("<br> <input type='checkbox'> options");
+	$('#optionadd').click(function() {
+		$("active").removeClass("active");
+		$(".optionTbody.active").append($('#optiontemplate').html());
+
 	});
-	function recalculate() {
-		var sum = 0;
-	  $("#tab tbody tr").each(function(idx, row) {
-	    var $el = $(row);
-			var unitPrice = parseInt($el.find(".unit-price").val(), 10);
-			var qty = parseInt($el.find(".qty").val());
-			if (!isNaN(unitPrice) && !isNaN(qty) ) {
-				var price = unitPrice * qty;
-				$el.find(".price").text(price);
-				sum = sum + price;
-			}
-		});
-		$("#sum").text(sum);
-	}
 
-	function initCalculator() {
+	$('#contentadd').click(function() {
+		$(".contentTbody").append($('#contenttemplate').html());
+	});
 
-		$('#del').click(function() {
-			if (confirm("정말 삭제하시겠습니까?")) {
-				var $els = $("tr input[type='checkbox']:checked");
-	      $els.each(function(idx, el) {
-	        $(el).parents("tr").empty();
-	      });
-				recalculate();
-			}
-		});
-		$('#add').click();
-	}
+	$('.contentdel').click(function(){
+		var self=$(this);
+
+		 $(this).parent("td").parent("tr").empty();
+
+
+	})
+
 })
