31dd7cdd035b82bb54d04a293e03bc406c6d53d5
pjt3591oo <pjt3591oo@naver.com>
2015-12-04 16:00:55 +0900

    설문 결과 객관식 부분 그래프로 보여주기 추가 함, 리커트척도 부분 그래프로 표현할 예정

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,35 +1,61 @@
 $('document').ready(function(){
   //alert('test');
 
+
+
   $.each($('.surveyQuestion'),function(index){
     var Graphs = new Array();
-
+    var op= new Array();
     var c = $('.surveyQuestion').eq(index).find('#myCanvas')[0];
 
     $.each($('.surveyQuestion'),function(index){
       var temp = $(this);
       $.each($('.surveyQuestion').eq(index).find('tr'),function(ind){
-        if($('.surveyQuestion').eq(index).attr('class')==='객관식'){
-          alert(ind);
+        if($('.surveyQuestion').eq(index).attr('class')==='surveyQuestion 객관식'){
+          var num=$('.surveyQuestion').eq(index).find('tr').eq(ind).find('td').data().id;
+          //alert(typeof Graphs[num]==="undefined");
+          if(typeof Graphs[num]==="undefined"){
+            Graphs[num]=1;
+            op[num]=$('.surveyQuestion').eq(0).find('tr').eq(0).find('td').text();
+          }else{
+            Graphs[num]+=1;
+          }
+          Graphs[num] = (typeof Graphs[num]==="undefined")?  0: Graphs[num]++ ;
+        //  alert(Graphs[num]+1);
         }
       })
       //$('.surveyQuestion').eq(index).find('#myCanvas')[0]
     })
 
-
-    var ctx = c.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillStyle = "#00FF00";
-    ctx.fillRect(0,0,50,100);
+    draw(Graphs, index,c,op );
 
   })
 
-
 })
 
-function Graph(option, num ){
+function draw(Graphs, index,c,op){
+  for(var i in Graphs){
+    //alert(Graphs[i]);
+    $('.surveyQuestion').eq(index).find('#myCanvas')[0].style.height="200";
+    var ctx = c.getContext("2d");
+    if(i%4==1){
+      ctx.fillStyle = "#F00000";
+    }else if(i%4==2){
+      ctx.fillStyle = "#0FF000";
+    }else if(i%4==3){
+      ctx.fillStyle = "#00FF00";
+    }else if(i%4==0){
+      ctx.fillStyle = "#000F00";
+    }
+    ctx.font = "10pt sans-serif";
 
-  this.PositionX=30*option;
-  this.PositionY=0;
-  this.height =10*num;
+    ctx.fillRect(20+10,60*i,30*Graphs[i],25);
+    if(i!=='0'){
+      ctx.fillText(Graphs[i]+'명', 2, 70*i);
+      ctx.fillText(op[i], 2, 70*i+30);
+    }else{
+      ctx.fillText(Graphs[i]+'명', 2, 70*i+15);
+      ctx.fillText(op[i], 2, 70*i+40);
+    }
+  }
 }
